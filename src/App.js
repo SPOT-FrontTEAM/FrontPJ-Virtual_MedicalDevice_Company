1d2a1289a3d1186aeea8904f2fd6f705d8515517
chanho <chjo1245@gmail.com>
2022-08-01 02:18:19 +0900

    create develop

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 
+import "swiper/css/bundle";
+import "./components/LandingPage/sections/LandingPage.css";
+
+//page
+import LandingPage from "./components/LandingPage/LandingPage";
+import Footer from "./components/_Common/Footer/Footer";
+import IrPage from "./components/IrPage/IrPage";
+
 function App() {
-  return <div className="App">test</div>;
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/ir" element={<IrPage />} />
+        </Routes>
+      <Footer />
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
