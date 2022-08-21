041a8cad91a8d574b11972ba2fa572fa903dee9f
hajung00 <gkwjd719@naver.com>
2022-08-21 14:25:40 +0900

    Auto stash before merge of "develop" and "origin/develop"

--- src/components/ExplainPage/CompanyAsk.js
+++ src/components/ExplainPage/CompanyAsk.js
@@ -3,7 +3,6 @@ import Divide from '../_Common/Divide';
 import Nav from '../_Common/Nav/Nav';
 import TitleImg from '../_Common/TilteImg/TitleImg';
 import Greeting from '../_Common/Greeting';
-import CompanyIntroduceList from './sections/CompanyIntroduceList';
 import './sections/ExplainPage.css';
 import AskContents from './sections/AskContents';
 
@@ -14,7 +13,6 @@ const CompanyAsk = () => {
     <div>
       <Nav />
       <TitleImg title='문의하기' type='Direction' />
-      <CompanyIntroduceList />
       <Divide division='회사소개' section='문의하기' />
       <Greeting title='삼성메디슨 홈페이지를 방문해 주셔서 감사합니다.' />
       <div className='ask-back'>
